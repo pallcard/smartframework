a7126a395ff14466cb83551eda131bf0b3736471
Liu, Curry <Curry.Liu@harman.com>
2019-08-21 18:25:10 +0800

    save classutil

--- src/main/java/cn/wishhust/framework/util/ClassUtil.java
+++ src/main/java/cn/wishhust/framework/util/ClassUtil.java
@@ -3,6 +3,28 @@ package cn.wishhust.framework.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ClassUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(PropsUtil.class);
+
+    public static ClassLoader getClassLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
+
+    public static Class<?> loadClass(String className, boolean isInitiallized) {
+        Class<?> cls;
+        try {
+            cls = Class.forName(className, isInitiallized, getClassLoader());
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("load class failure", e);
+            throw new RuntimeException(e);
+        }
+        return cls;
+    }
+
+    public static Set<Class<?>> getClassSet(String packageName) {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+    }
 }
