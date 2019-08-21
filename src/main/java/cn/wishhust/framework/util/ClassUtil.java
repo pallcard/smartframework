e6bf0dab336f0646058f19522c72aaabf7bae207
Liu, Curry <Curry.Liu@harman.com>
2019-08-21 18:02:47 +0800

    增加util

---
+++ src/main/java/cn/wishhust/framework/util/ClassUtil.java
@@ -0,0 +1,8 @@
+package cn.wishhust.framework.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ClassUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropsUtil.class);
+}
---
+++ src/main/java/cn/wishhust/framework/util/StringUtil.java
@@ -0,0 +1,28 @@
+package cn.wishhust.framework.util;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 字符串工具类
+ */
+public final class StringUtil {
+
+
+    /**
+     * 字符串是否为空
+     * @param str
+     * @return
+     */
+    public static boolean isEmpty(String str) {
+        if (str != null) {
+            str = str.trim();
+        }
+        return StringUtils.isEmpty(str);
+    }
+
+    public static boolean isNotEmpty(String str) {
+        return !isEmpty(str);
+    }
+
+}
